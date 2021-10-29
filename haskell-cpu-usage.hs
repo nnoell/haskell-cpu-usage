2a27df16eedd22fadbf757cd9d2b01c8dcd750ce
Julian Bouzas <julian.bouzas@collabora.com>
2021-10-29 11:14:11 -0400

    Make default parameters configurable

--- haskell-cpu-usage.hs
+++ haskell-cpu-usage.hs
@@ -1,14 +1,14 @@
---------------------------------------------------------------------------------------------
--- Author      : Nnoell <nnoell3[at]gmail.com>                                            --
--- Stability   : Stable                                                                   --
--- Desc        : Simple haskell CPU usage monitor                                         --
--- Build       : $ ghc haskell-cpu-usage.hs -o haskell-cpu-usage.out                      --
--- Usage       : haskell-cpu-usage.out <number-of-cores>                                  --
---               1 -> [cpu]                                                               --
---               2 -> [cpu, cpu1]                                                         --
---               3 -> [cpu, cpu1, cpu2]                                                   --
---               N -> [cpu, cpu1, cpu2, ..., cpuN-1]                                      --
---------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------
+--- Author      : Julian Bouzas <nnoell3[at]gmail.com>                                     --
+--- License     : BSD3-style (see LICENSE)                                                 --
+--- Stability   : Stable                                                                   --
+--- Desc        : Simple haskell CPU usage monitor                                         --
+--- Build       : $ ghc haskell-cpu-usage.hs -o haskell-cpu-usage.out                      --
+--- Usage       : haskell-cpu-usage.out <num-cores> <regresh-rate> <output-file>           --
+---               Example: ./haskell-cpu-usage.out 5 1 "/tmp/haskell-cpu-usage.txt"        --
+---------------------------------------------------------------------------------------------
+
+module Main where
 
 -- Useful imports
 import Control.Concurrent (threadDelay)
@@ -20,15 +20,26 @@ defNumCores :: Int
 defNumCores = 5
 
 -- | Refresh rate (in seconds)
-delay :: Int
-delay = 1
+defRefreshRate :: Int
+defRefreshRate = 1
 
 -- | Where to write cpu percent
-cpuPercFile :: String
-cpuPercFile = "/tmp/haskell-cpu-usage.txt"
+defOutputFile :: String
+defOutputFile = "/tmp/haskell-cpu-usage.txt"
 
 
 --------------------------------------------------------------------------------------------
+
+data Settings = Settings { settingsNumCores :: Int
+                         , settingsRefreshRate :: Int
+                         , settingsOutputFile :: String
+                         }
+
+parseSettings :: [String] -> Settings
+parseSettings [] = Settings defNumCores defRefreshRate defOutputFile
+parseSettings [x] = Settings (read x::Int) defRefreshRate defOutputFile
+parseSettings [x,y] = Settings (read x::Int) (read y::Int) defOutputFile
+parseSettings [x,y,z] = Settings (read x::Int) (read y::Int) z
 
 -- | Get /proc/stat values of a specific cpu
 cpuInfo :: Int -- ^ number of cores
@@ -36,7 +47,7 @@ cpuInfo :: Int -- ^ number of cores
 cpuInfo ncpus = do
   c <- readFile "/proc/stat"
   let getCoresInfo = map (dropWhile (/= ' ')) $ take ncpus $ lines c
-  return $ map (map read) $ map words $ getCoresInfo
+  return $ map (map read . words) getCoresInfo
 
 
 -- | Get cpuPerc with its respective idle and total amount of time: [idle,total,cpuPerc]
@@ -44,34 +55,38 @@ getPerc :: [Integer] -- ^ specific cpu values of /proc/stat
         -> Integer   -- ^ previous idle value
         -> Integer   -- ^ previous total value
         -> [Integer]
-getPerc cpu pidle ptotal = idle:total:diffUsage:[]
+getPerc cpu pidle ptotal = [idle, total, diffUsage]
   where
     idle = cpu !! 3
     total = sum cpu
     diffIdle  = idle - pidle
     diffTotal = total - ptotal
-    diffUsage = (100 * (diffTotal - diffIdle) `div` diffTotal)
+    diffUsage = 100 * (diffTotal - diffIdle) `div` diffTotal
 
--- | Transforms a list of Integers to one line String separating values by Whitespaces
+-- | Transforms a list of Integers to one line String separating values with spaces
 percToStr :: [Integer] -- ^ List of Integers
-             -> String
-percToStr xs = (concat [ (show x) ++ " " | x <- xs ]) ++ "\n"
+          -> String
+percToStr xs = (concat [ show x ++ " " | x <- xs ]) ++ "\n"
 
 -- | Recursive cpu usage calculator
-recWritePerc :: Int       -- ^ number of cores
+recWritePerc :: Settings  -- ^ settings
              -> [Integer] -- ^ previous idle values of all cores
              -> [Integer] -- ^ previous total values of all cores
              -> IO ()
-recWritePerc ncpus pis pts = do
-  cpus <- cpuInfo ncpus
+recWritePerc settings pis pts = do
+  cpus <- cpuInfo $ settingsNumCores settings
   let results = zipWith (!!) [ zipWith (getPerc cpu) pis pts | cpu <- cpus ] [0..]
-  writeFile cpuPercFile $ percToStr $ map (!!2) results
-  threadDelay $ delay * 1000000
-  recWritePerc ncpus (map (!!0) results) (map (!!1) results)
+  let resultsStr = percToStr $ map (!!2) results
+  if null $ settingsOutputFile settings then
+    putStr resultsStr
+  else
+    writeFile (settingsOutputFile settings) resultsStr
+  threadDelay $ settingsRefreshRate settings * 1000000
+  recWritePerc settings (map (!!0) results) (map (!!1) results)
 
 -- | Main
 main :: IO ()
 main = do
   args <- getArgs
-  let argValue = if null args then defNumCores else (read (head args)::Int)
-  recWritePerc argValue [0,0..0] [0,0..0]
+  let settings = parseSettings args
+  recWritePerc settings [0,0..0] [0,0..0]
